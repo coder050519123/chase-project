99847e063454cb52b4adbbae45401f4a399cb987
natashamartinas <nms2182@columbia.edu>
2023-06-23 13:51:06 -0400

    Restructured Theater class and deleted the LocalDateProvider.java class since it was unnecessary

--- src/main/java/com/jpmc/theater/Theater.java
+++ src/main/java/com/jpmc/theater/Theater.java
@@ -1,55 +1,103 @@
 package com.jpmc.theater;
 
+import java.math.BigDecimal;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class Theater {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    LocalDateProvider provider;
+/**
+ * This class represents the theater containing a schedule of movie showings in the day.
+ * The theater object holds a list of showings containing the respective movies.
+ *
+ * @author natashamartinas
+ */
+public class Theater {
     private List<Showing> schedule;
 
-    public Theater(LocalDateProvider provider) {
-        this.provider = provider;
-
-        Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, 1);
-        Movie turningRed = new Movie("Turning Red", Duration.ofMinutes(85), 11, 0);
-        Movie theBatMan = new Movie("The Batman", Duration.ofMinutes(95), 9, 0);
-        schedule = List.of(
-            new Showing(turningRed, 1, LocalDateTime.of(provider.currentDate(), LocalTime.of(9, 0))),
-            new Showing(spiderMan, 2, LocalDateTime.of(provider.currentDate(), LocalTime.of(11, 0))),
-            new Showing(theBatMan, 3, LocalDateTime.of(provider.currentDate(), LocalTime.of(12, 50))),
-            new Showing(turningRed, 4, LocalDateTime.of(provider.currentDate(), LocalTime.of(14, 30))),
-            new Showing(spiderMan, 5, LocalDateTime.of(provider.currentDate(), LocalTime.of(16, 10))),
-            new Showing(theBatMan, 6, LocalDateTime.of(provider.currentDate(), LocalTime.of(17, 50))),
-            new Showing(turningRed, 7, LocalDateTime.of(provider.currentDate(), LocalTime.of(19, 30))),
-            new Showing(spiderMan, 8, LocalDateTime.of(provider.currentDate(), LocalTime.of(21, 10))),
-            new Showing(theBatMan, 9, LocalDateTime.of(provider.currentDate(), LocalTime.of(23, 0)))
-        );
+    /**
+     * Constructs a new Theater object with the provided schedule list of showings
+     * @param schedule - List of movie showings for the day
+     */
+    public Theater(List<Showing> schedule) {
+        this.schedule = schedule;
     }
 
-    public Reservation reserve(Customer customer, int sequence, int howManyTickets) {
+    /**
+     * @return the schedule list of showings for the theater for the day
+     */
+    public List<Showing> getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * @param schedule - method to change the schedule list of the theater for the day
+     */
+    public void setSchedule(List<Showing> schedule) {
+        this.schedule = schedule;
+    }
+
+    /**
+     * Method to create a reservation in the theater based on the day's schedule
+     * @param customer - customer initiating reservation request
+     * @param sequence - the selected showing sequence
+     * @param ticketAmount - the requested party size of the reservation
+     * @return the reservation object if all the input parameters are valid
+     */
+    public Reservation createReservation(Customer customer, int sequence, int ticketAmount) {
+        if (ticketAmount <= 0) {
+            throw new IllegalArgumentException("Ticket amount cannot be less than 1!");
+        }
         Showing showing;
         try {
             showing = schedule.get(sequence - 1);
-        } catch (RuntimeException ex) {
-            ex.printStackTrace();
-            throw new IllegalStateException("not able to find any showing for given sequence " + sequence);
+        } catch (IndexOutOfBoundsException ex) {
+            throw new IllegalStateException("Not able to find any showing for given sequence " + sequence + " and exception is: " + ex.getMessage());
         }
-        return new Reservation(customer, showing, howManyTickets);
+        return new Reservation(customer, showing, ticketAmount);
     }
 
+    /**
+     * Method to print the theater schedule in a pretty format
+     */
     public void printSchedule() {
-        System.out.println(provider.currentDate());
+        if (schedule.isEmpty()) {
+            System.out.println("No shows scheduled.");
+            return;
+        }
+        System.out.println(LocalDate.now());
         System.out.println("===================================================");
         schedule.forEach(s ->
-                System.out.println(s.getSequenceOfTheDay() + ": " + s.getStartTime() + " " + s.getMovie().getTitle() + " " + humanReadableFormat(s.getMovie().getRunningTime()) + " $" + s.getMovieFee())
+                System.out.println(s.getSequenceOfTheDay() + ": " + s.getShowStartTime() + " " + s.getMovie().getTitle() + " " + humanReadableFormat(s.getMovie().getRunningTime()) + " $" + s.getMovie().getTicketPrice())
         );
         System.out.println("===================================================");
     }
 
+    /**
+     * Method to print the theater schedule in a JSON format
+     */
+    public void printScheduleToJson() {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, List<Showing>> scheduleMap = new HashMap<>();
+        scheduleMap.put(LocalDate.now().toString(), schedule);
+        try {
+            String scheduleJsonWithDate = mapper.writeValueAsString(scheduleMap);
+            System.out.println(scheduleJsonWithDate);
+        } catch (JsonProcessingException ex) {
+            System.out.println("Could not convert schedule list with date to JSON: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Method to handle the movie's duration to be printed in a readable format
+     */
     public String humanReadableFormat(Duration duration) {
         long hour = duration.toHours();
         long remainingMin = duration.toMinutes() - TimeUnit.HOURS.toMinutes(duration.toHours());
@@ -57,7 +105,9 @@ public class Theater {
         return String.format("(%s hour%s %s minute%s)", hour, handlePlural(hour), remainingMin, handlePlural(remainingMin));
     }
 
-    // (s) postfix should be added to handle plural correctly
+    /**
+     * Method to add plurality to the duration of the movie
+     */
     private String handlePlural(long value) {
         if (value == 1) {
             return "";
@@ -67,8 +117,30 @@ public class Theater {
         }
     }
 
+    /**
+     * This is the main method that creates a Theater object and prints the schedule of the showings of the day.
+     * The method creates instances of each Movie to be shown (all fields would be immutable once created).
+     * Then each Movie created will be added to a specific Showing for the day (fields are also immutable).
+     * Each Showing will be added to a list called the Schedule which will be used to instantiate the Theater object.
+     * The printSchedule() method is called to display the schedule of the theater for the day.
+     */
     public static void main(String[] args) {
-        Theater theater = new Theater(LocalDateProvider.singleton());
+        Movie spiderMan = new Movie("Spider-Man: No Way Home", "Spider-Man movie description.", Duration.ofMinutes(90), BigDecimal.valueOf(12.5), 1);
+        Movie turningRed = new Movie("Turning Red", "This is a Disney movie.", Duration.ofMinutes(85), BigDecimal.valueOf(11), 0);
+        Movie theBatMan = new Movie("The Batman", "This is a DC Comics movie", Duration.ofMinutes(95), BigDecimal.valueOf(9), 0);
+
+        Theater theater = new Theater(List.of(
+                new Showing(turningRed, 1, LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 0))),
+                new Showing(spiderMan, 2, LocalDateTime.of(LocalDate.now(), LocalTime.of(11, 0))),
+                new Showing(theBatMan, 3, LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 50))),
+                new Showing(turningRed, 4, LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 30))),
+                new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(16, 10))),
+                new Showing(theBatMan, 6, LocalDateTime.of(LocalDate.now(), LocalTime.of(17, 50))),
+                new Showing(turningRed, 7, LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 30))),
+                new Showing(spiderMan, 8, LocalDateTime.of(LocalDate.now(), LocalTime.of(21, 10))),
+                new Showing(theBatMan, 9, LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 0)))
+        ));
+
         theater.printSchedule();
     }
 }
--- src/test/java/com/jpmc/theater/TheaterTests.java
+++ src/test/java/com/jpmc/theater/TheaterTests.java
@@ -1,22 +1,182 @@
 package com.jpmc.theater;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+/**
+ * Unit and Integration tests for Theater class.
+ */
 public class TheaterTests {
-    @Test
-    void totalFeeForCustomer() {
-        Theater theater = new Theater(LocalDateProvider.singleton());
-        Customer john = new Customer("John Doe", "id-12345");
-        Reservation reservation = theater.reserve(john, 2, 4);
-//        System.out.println("You have to pay " + reservation.getTotalFee());
-        assertEquals(reservation.totalFee(), 50);
+    /**
+     * PrintStream and ByteArrayOutputStream are used to test the System.out.println methods called in the class
+     */
+    private PrintStream originalSystemOut;
+    private ByteArrayOutputStream outputStream;
+    private Theater theater;
+    private Customer customer;
+    private List<Showing> schedule;
+
+    /**
+     * This method sets up the environment before each test case runs.
+     */
+    @BeforeEach
+    public void setUp() {
+        schedule = new ArrayList<>();
+        Movie movie1 = new Movie("Test Movie 1", "Test Movie Desc 1", Duration.ofMinutes(100), BigDecimal.valueOf(20), 0);
+        Movie movie2 = new Movie("Test Movie 2", "Test Movie Desc 2", Duration.ofMinutes(100), BigDecimal.valueOf(22), 1);
+
+        schedule.add(new Showing(movie1, 1, LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 0))));
+        schedule.add(new Showing(movie2, 2, LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0))));
+
+        theater = new Theater(schedule);
+        customer = new Customer("John Doe", "customer-random-id");
+
+        outputStream = new ByteArrayOutputStream();
+        originalSystemOut = System.out;
+        System.setOut(new PrintStream(outputStream));
     }
 
+    /**
+     * This is a case to see that the schedule getter returns the right object
+     * Test data includes:
+     * - Theater with 2 movies in the schedule
+     * Expected result:
+     * - The schedule returned should have 2 movies
+     */
     @Test
-    void printMovieSchedule() {
-        Theater theater = new Theater(LocalDateProvider.singleton());
+    public void testGetTheaterSchedule() {
+        Assertions.assertEquals(2, theater.getSchedule().size());
+    }
+
+    /**
+     * This is a case to test a valid reservation being created for the Theater
+     * Test data includes:
+     * - Valid customer, sequence, and ticketAmount inputs
+     * - Theater with 2 movies in schedule
+     * Expected result:
+     * - The correct reservation object being created that is non-null
+     */
+    @Test
+    public void testCreateReservation_ValidReservation() {
+        int sequence = 2;
+        int ticketAmount = 3;
+        Reservation reservation = theater.createReservation(customer, sequence, ticketAmount);
+        Assertions.assertNotNull(reservation);
+        Assertions.assertEquals(customer, reservation.getCustomer());
+        Assertions.assertEquals(schedule.get(sequence - 1).getMovie(), reservation.getShowing().getMovie());
+        Assertions.assertEquals(ticketAmount, reservation.getAudienceCount());
+    }
+
+    /**
+     * This is a case to test an invalid reservation attempted to be created for the Theater
+     * Test data includes:
+     * - Invalid sequence input, and valid ticketAmount input
+     * Expected result:
+     * - The method throws an IllegalStateException due to the sequence being greater than the number of showings in the schedule
+     */
+    @Test
+    public void testCreateReservation_InvalidReservation_IncorrectSequence_ThrowsIllegalStateException() {
+        int sequence = 15;
+        int ticketAmount = 3;
+        Assertions.assertThrows(IllegalStateException.class, () -> theater.createReservation(customer, sequence, ticketAmount));
+    }
+
+    /**
+     * This is a case to test an invalid reservation attempted to be created for the Theater
+     * Test data includes:
+     * - Invalid ticketAmount input, and valid sequence input
+     * Expected result:
+     * - The method throws an IllegalArgumentException due to the ticketAmount being less than 1
+     */
+    @Test
+    public void testCreateReservation_InvalidReservation_IncorrectTicketAmount_ThrowsIllegalArgumentException() {
+        int sequence = 1;
+        int ticketAmount = -3;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> theater.createReservation(customer, sequence, ticketAmount));
+    }
+
+    /**
+     * This is an integration test to create a reservation from the theater and calculate the total fee for the reservation
+     * Test data includes:
+     * - Valid customer, sequence, and ticketAmount inputs
+     * - Theater with 2 movies in schedule
+     * - Showing chosen is eligible for special movie discount
+     * Expected result:
+     * - The method should return the total reservation fee to be $70.4
+     */
+    @Test
+    public void testIntegrationTheaterTest_ValidReservation_SpecialMovieDiscount() {
+        int sequence = 2;
+        int ticketAmount = 4;
+        Reservation reservation = theater.createReservation(customer, sequence, ticketAmount);
+        Assertions.assertEquals(0, reservation.calculateTotalReservationFee().compareTo(BigDecimal.valueOf(70.4)));
+    }
+
+    /**
+     * This is a test to see if the schedule print method works as expected
+     * This uses PrintStream and ByteArrayOutputStream
+     * Test data includes:
+     * - Theater with an empty schedule
+     * Expected result:
+     * - The method should return with a message saying no shows are scheduled.
+     */
+    @Test
+    public void testPrintMovieSchedule_NoSchedule() {
+        Theater newTheaterEmptySchedule = new Theater(Collections.emptyList());
+        newTheaterEmptySchedule.printSchedule();
+        String output = outputStream.toString();
+
+        String expectedResult = "No shows scheduled.";
+        Assertions.assertEquals(expectedResult, output.trim());
+    }
+
+    /**
+     * This is another test to see if the schedule print method works as expected
+     * This uses PrintStream and ByteArrayOutputStream
+     * Test data includes:
+     * - Theater with 2 movies in the schedule
+     * Expected result:
+     * - The method should return with the correct schedule printed with 2 movies
+     */
+    @Test
+    public void testPrintMovieSchedule_ScheduleExists() {
         theater.printSchedule();
+        String output = outputStream.toString();
+
+        String expectedResult = LocalDate.now() + System.lineSeparator() +
+                "===================================================" + System.lineSeparator() +
+                "1: " + LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 0)) + " Test Movie 1 (1 hour 40 minutes) $20" + System.lineSeparator() +
+                "2: " + LocalDateTime.of(LocalDate.now(), LocalTime.of(10, 0)) + " Test Movie 2 (1 hour 40 minutes) $22" + System.lineSeparator() +
+                "===================================================" + System.lineSeparator();
+        Assertions.assertEquals(expectedResult, output);
+    }
+
+    /**
+     * This is a test to see if the schedule print to JSON method works as expected
+     * This uses PrintStream and ByteArrayOutputStream
+     * Test data includes:
+     * - Theater with 2 movies in the schedule
+     * Expected result:
+     * - The method should return with the correct schedule printed with 2 movies but in a JSON format and encapsulated by the date
+     */
+    @Test
+    public void testPrintMovieScheduleToJson() {
+        theater.printScheduleToJson();
+        String output = outputStream.toString();
+
+        String expectedResult = "{\"2023-06-23\":[{\"movie\":{\"title\":\"Test Movie 1\",\"description\":\"Test Movie Desc 1\",\"runningTime\":{\"seconds\":6000,\"nano\":0,\"negative\":false,\"zero\":false,\"units\":[\"SECONDS\",\"NANOS\"]},\"ticketPrice\":20,\"specialCode\":0},\"sequenceOfTheDay\":1,\"showStartTime\":{\"year\":2023,\"monthValue\":6,\"dayOfMonth\":23,\"hour\":8,\"minute\":0,\"second\":0,\"nano\":0,\"dayOfWeek\":\"FRIDAY\",\"dayOfYear\":174,\"month\":\"JUNE\",\"chronology\":{\"id\":\"ISO\",\"calendarType\":\"iso8601\"}},\"finalShowingPrice\":17.00},{\"movie\":{\"title\":\"Test Movie 2\",\"description\":\"Test Movie Desc 2\",\"runningTime\":{\"seconds\":6000,\"nano\":0,\"negative\":false,\"zero\":false,\"units\":[\"SECONDS\",\"NANOS\"]},\"ticketPrice\":22,\"specialCode\":1},\"sequenceOfTheDay\":2,\"showStartTime\":{\"year\":2023,\"monthValue\":6,\"dayOfMonth\":23,\"hour\":10,\"minute\":0,\"second\":0,\"nano\":0,\"dayOfWeek\":\"FRIDAY\",\"dayOfYear\":174,\"month\":\"JUNE\",\"chronology\":{\"id\":\"ISO\",\"calendarType\":\"iso8601\"}},\"finalShowingPrice\":17.60}]}";
+        Assertions.assertEquals(expectedResult, output.trim());
     }
 }
